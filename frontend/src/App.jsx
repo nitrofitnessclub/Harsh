9df6c3a2bedc311490152ca99d879af9ac641489
Hacklivly <128738691+hacklivly@users.noreply.github.com>
2025-04-13 08:42:34 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,10 +5,6 @@ import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
-import PrivacyPolicy from "./PrivacyPolicy";
-import ContactUs from "./ContactUs";
-import TermsOfService from "./TermsOfService";
-import AboutUs from "./AboutUs";
 
 
 import { Routes, Route, Navigate } from "react-router-dom";
@@ -47,10 +43,6 @@ const App = () => {
         <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to="/" />} />
         <Route path="/login" element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
         <Route path="/settings" element={<SettingsPage />} />
-        <Route path="/AboutUs" element={<About/>} />
-        <Route path="/PrivacyPolicy" element={<Privecy />} />
-        <Route path="/ContactUs" element={<Contact />} />
-        <Route path="/TermsOfService" element={<Terms />} />
         <Route path="/profile" element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
 
